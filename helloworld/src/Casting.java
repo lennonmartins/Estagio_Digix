ad6dc0bcf73f6fff9efeb7783540f6f42f707262
Lennon Martins <lennonmcarlos@gmail.com>
2022-06-22 17:35:25 -0400

    iniciando com testes

--- helloworld/src/Casting.java
+++
@@ -1,8 +0,0 @@
-public class Casting {
-    public static void main(String[] args) {
-        double d =  3.1415;
-        int i = (int) d;
-        System.out.println(i);
-        System.out.println(d);
-    }
-}
